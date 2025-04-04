685d43c3bcfe772a04066d90042ed97f77426749
philiph915 <philip.h915@gmail.com>
2025-04-04 00:30:01 -0400

    add new colors to the game

--- include/colors.h
+++ include/colors.h
@@ -12,5 +12,7 @@ extern const Color yellow;
 extern const Color purple; 
 extern const Color cyan;   
 extern const Color blue;   
+extern const Color lightBlue;
+extern const Color darkBlue;
 
 std::vector<Color>  GetCellColors();
